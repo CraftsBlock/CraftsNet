66816ee0c472b002390b2ace1cf231c9a7ed1862
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2024-12-30 14:30:31 +0100

    [CH] Load the type of body parser during registration

--- src/main/java/de/craftsblock/craftsnet/api/http/body/BodyRegistry.java
+++ src/main/java/de/craftsblock/craftsnet/api/http/body/BodyRegistry.java
@@ -1,13 +1,12 @@
 package de.craftsblock.craftsnet.api.http.body;
 
-import de.craftsblock.craftsnet.api.http.body.bodies.JsonBody;
-import de.craftsblock.craftsnet.api.http.body.bodies.MultipartFormBody;
-import de.craftsblock.craftsnet.api.http.body.bodies.StandardFormBody;
 import de.craftsblock.craftsnet.api.http.body.parser.JsonBodyParser;
 import de.craftsblock.craftsnet.api.http.body.parser.MultipartFormBodyParser;
 import de.craftsblock.craftsnet.api.http.body.parser.StandardFormBodyParser;
+import de.craftsblock.craftsnet.utils.ReflectionUtils;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -17,7 +16,7 @@ import java.util.concurrent.ConcurrentHashMap;
  *
  * @author CraftsBlock
  * @author Philipp Maywald
- * @version 1.0.0
+ * @version 1.1.0
  * @see BodyParser
  * @since 3.0.4-SNAPSHOT
  */
@@ -30,20 +29,19 @@ public class BodyRegistry {
      * This constructor initializes the registry with parsers for JSON, multipart form, and standard form bodies.
      */
     public BodyRegistry() {
-        register(JsonBody.class, new JsonBodyParser());
-        register(MultipartFormBody.class, new MultipartFormBodyParser());
-        register(StandardFormBody.class, new StandardFormBodyParser());
+        register(new JsonBodyParser());
+        register(new MultipartFormBodyParser());
+        register(new StandardFormBodyParser());
     }
 
     /**
      * Registers a body parser for a specific body type.
      *
-     * @param type       The class representing the body type.
      * @param bodyParser The body parser for the specified body type.
      * @param <T>        The type of body being parsed.
      */
-    public <T extends Body> void register(@NotNull Class<T> type, @NotNull BodyParser<T> bodyParser) {
-        this.bodyParser.put(type, bodyParser);
+    public <T extends Body> void register(@NotNull BodyParser<T> bodyParser) {
+        this.bodyParser.put(Objects.requireNonNull(ReflectionUtils.extractGeneric(bodyParser.getClass(), BodyParser.class)), bodyParser);
     }
 
     /**
