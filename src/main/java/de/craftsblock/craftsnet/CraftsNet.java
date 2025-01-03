6d31f247ad20793c06873363b904973e7b9e162e
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-01-03 19:15:02 +0100

    [CH] Bump version to 3.2.2-SNAPSHOT

--- src/main/java/de/craftsblock/craftsnet/CraftsNet.java
+++ src/main/java/de/craftsblock/craftsnet/CraftsNet.java
@@ -50,7 +50,7 @@ import java.util.jar.JarFile;
 public class CraftsNet {
 
     // Global variables
-    public static final String version = "3.2.1-SNAPSHOT";
+    public static final String version = "3.2.2-SNAPSHOT";
 
     // Local instance
     private CraftsNetBuilder builder;
