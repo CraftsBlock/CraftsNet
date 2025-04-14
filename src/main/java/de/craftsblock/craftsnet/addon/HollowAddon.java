e7f908ed17a6051aac016dce5a29fce9d0c258c6
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-04-14 20:02:20 +0200

    [FIX] Fixed HollowAddon not initializable by the AddonLoader

--- src/main/java/de/craftsblock/craftsnet/addon/HollowAddon.java
+++ src/main/java/de/craftsblock/craftsnet/addon/HollowAddon.java
@@ -1,21 +1,29 @@
 package de.craftsblock.craftsnet.addon;
 
+import de.craftsblock.craftsnet.addon.loaders.AddonLoader;
+import de.craftsblock.craftsnet.utils.ReflectionUtils;
+
 /**
  * An instance of {@link Addon} which is only capable of holding dummy
  * data of an addon that did not specify a valid main class.
  *
  * @author Philipp Maywald
  * @author CraftsBlock
- * @version 1.0.0
+ * @version 1.0.1
  * @see Addon
  * @since 3.3.5-SNAPSHOT
  */
 public final class HollowAddon extends Addon {
 
     /**
-     * Protected constructor to prevent direct initialization.
+     * Constructor which checks the caller class to prevent direct initialization.
+     *
+     * @throws RuntimeException If this constructor is instantiated by another class as {@link AddonLoader}.
      */
-    HollowAddon() {
+    public HollowAddon() {
+        Class<?> clazz = ReflectionUtils.getCallerClass();
+        if (AddonLoader.class.isAssignableFrom(clazz)) return;
+        throw new RuntimeException(clazz.getName() + " is not permitted to initialize a " + HollowAddon.class.getSimpleName());
     }
 
 }
