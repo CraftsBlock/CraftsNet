240288eb976a824804a90c2fa2168eea91f06595
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-04-21 12:40:24 +0200

    [FIX] Corrected the javadoc of the PlainLogger

--- src/main/java/de/craftsblock/craftsnet/logging/impl/PlainLogger.java
+++ src/main/java/de/craftsblock/craftsnet/logging/impl/PlainLogger.java
@@ -5,13 +5,9 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * An implementation of the {@link Logger} interface that performs no operations.
- * This can be used as a placeholder or default logger where logging functionality
- * is not required or should be ignored.
- * <p>
- * The {@code PlainLogger} simply accepts log messages and errors but does not process
- * them in any way. It is a no-op logger.
- * </p>
+ * An implementation of the {@link Logger} interface that prints the result
+ * in the {@link System#out} or the {@link System#err} stream, without any
+ * further formating.
  *
  * @author CraftsBlock
  * @author Philipp Maywald
@@ -21,18 +17,18 @@ import org.jetbrains.annotations.Nullable;
 public record PlainLogger(Logger previous) implements Logger {
 
     /**
-     * Constructs an {@code PlainLogger} with a reference to a previous {@code Logger}.
+     * Constructs an {@link PlainLogger} with a reference to a previous {@link Logger}.
      *
-     * @param previous the previous {@code Logger} in the chain, can be used for chaining or fallback purposes.
+     * @param previous the previous {@link Logger} in the chain, can be used for chaining or fallback purposes.
      */
     public PlainLogger {
     }
 
     /**
-     * Returns the previous {@code Logger} in the chain.
-     * This can be used to access the logger that was previously used before this {@code PlainLogger}.
+     * Returns the previous {@link Logger} in the chain.
+     * This can be used to access the logger that was previously used before this {@link PlainLogger}.
      *
-     * @return the previous {@code Logger} instance.
+     * @return the previous {@link Logger} instance.
      */
     public Logger previous() {
         return this.previous;
@@ -40,7 +36,6 @@ public record PlainLogger(Logger previous) implements Logger {
 
     /**
      * Logs an informational message.
-     * This implementation does nothing.
      *
      * @param text the informational message to be logged, can be null.
      */
@@ -51,7 +46,6 @@ public record PlainLogger(Logger previous) implements Logger {
 
     /**
      * Logs a warning message.
-     * This implementation does nothing.
      *
      * @param text the warning message to be logged, can be null.
      */
@@ -62,7 +56,6 @@ public record PlainLogger(Logger previous) implements Logger {
 
     /**
      * Logs an error message.
-     * This implementation does nothing.
      *
      * @param text the error message to be logged, can be null.
      */
@@ -73,7 +66,6 @@ public record PlainLogger(Logger previous) implements Logger {
 
     /**
      * Logs an error with a throwable.
-     * This implementation does nothing.
      *
      * @param throwable the {@link Throwable} to be logged, must not be null.
      */
@@ -84,7 +76,6 @@ public record PlainLogger(Logger previous) implements Logger {
 
     /**
      * Logs an error with a throwable and an additional comment.
-     * This implementation does nothing.
      *
      * @param throwable the {@link Throwable} to be logged, must not be null.
      * @param comment   an additional comment or message to be logged, can be null.
@@ -97,7 +88,6 @@ public record PlainLogger(Logger previous) implements Logger {
 
     /**
      * Logs a debug message.
-     * This implementation does nothing.
      *
      * @param text the debug message to be logged, can be null.
      */
@@ -107,12 +97,10 @@ public record PlainLogger(Logger previous) implements Logger {
     }
 
     /**
-     * Creates a new instance of {@code PlainLogger} with a specified name.
-     * This implementation ignores the provided name and returns a new {@code PlainLogger}
-     * with the same previous logger.
+     * Returns this instance of the {@link PlainLogger}.
      *
-     * @param name the name for the new logger instance.
-     * @return a new instance of {@code PlainLogger} with the same previous logger.
+     * @param name the name for the new logger instance. (Not used in any way!)
+     * @return the current instance of this {@link PlainLogger}.
      */
     @Override
     public Logger cloneWithName(String name) {
