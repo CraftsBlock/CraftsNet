9b76f5e16dd7507a6eb8583e7c54dce8126cdb9f
Philipp <62473021+CrAfTsArMy@users.noreply.github.com>
2025-09-26 20:36:06 +0200

    fix: Print tab before the ascii color

--- src/main/java/de/craftsblock/craftsnet/logging/LogLevel.java
+++ src/main/java/de/craftsblock/craftsnet/logging/LogLevel.java
@@ -6,7 +6,7 @@ package de.craftsblock.craftsnet.logging;
  *
  * @author CraftsBlock
  * @author Philipp Maywald
- * @version 1.0.0
+ * @version 1.0.1
  * @since 3.1.0-SNAPSHOT
  */
 public enum LogLevel {
@@ -15,31 +15,31 @@ public enum LogLevel {
      * Debug level for detailed debugging information.
      * Color-coded light blue.
      */
-    DEBUG("\u001b[38;5;147mDEBUG\u001b[0m\t"),
+    DEBUG("\u001b[38;5;147mDEBUG\t\u001b[0m"),
 
     /**
      * Informational level for general information messages.
      * Color-coded blue.
      */
-    INFO("\u001b[34;1mINFO\u001b[0m\t"),
+    INFO("\u001b[34;1mINFO\t\u001b[0m"),
 
     /**
      * Warning level for potential issues or important notices.
      * Color-coded yellow.
      */
-    WARNING("\u001b[33mWARN\u001b[0m\t"),
+    WARNING("\u001b[33mWARN\t\u001b[0m"),
 
     /**
      * Error level for error messages indicating problems.
      * Color-coded red.
      */
-    ERROR("\u001b[31;1mERROR\u001b[0m\t"),
+    ERROR("\u001b[31;1mERROR\t\u001b[0m"),
 
     /**
      * Exception level for fatal errors and exceptions.
      * Color-coded red.
      */
-    EXCEPTION("\u001b[31;1mFATAL\u001b[0m\t");
+    EXCEPTION("\u001b[31;1mFATAL\t\u001b[0m");
 
     private final String prefix;
 
